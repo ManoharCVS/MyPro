e7b5be4e842153a1ac7057a76b57d7ef86d7497e
manohar7789 <manohar7789@gmail.com>
2015-11-16 17:24:26 +0530

    new change

--- MyPro/src/com/dev/Myclass.java
+++ MyPro/src/com/dev/Myclass.java
@@ -5,7 +5,7 @@ public class Myclass
 public static void main(String args[])
 {
 	System.out.println("HI=======");
-    ////System.out.println("Hello=======");
+    System.out.println("Hello=======");
 	
 	
 }
