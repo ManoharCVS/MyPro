3a765c0d824f67f2c96286327eb310858df7b9d7
ManoharCVS <manohar@celestialv.com>
2015-11-16 17:30:26 +0530

    Update Myclass.java

--- MyPro/src/com/dev/Myclass.java
+++ MyPro/src/com/dev/Myclass.java
@@ -4,7 +4,7 @@ public class Myclass
 {
 public static void main(String args[])
 {
-	System.out.println("HI=======");
+    System.out.println("HI=======");
     System.out.println("Hello=======");
     System.out.println("how r u...?");
 	
